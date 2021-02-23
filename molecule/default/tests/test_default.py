804085438c20ccbada494fcb3a89688ca83abbcf
Bo.Sch <bodo@boone-schulz.de>
2021-02-23 16:01:09 +0100

    use new molecule structure and tests (#1)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,57 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
 import pytest
 import os
-import yaml
 import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def base_directory():
+    cwd = os.getcwd()
+    pp.pprint(cwd)
+    pp.pprint(os.listdir(cwd))
+
+    if('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
 @pytest.fixture()
-def AnsibleDefaults():
-    with open("../../defaults/main.yml", 'r') as stream:
-        return yaml.load(stream)
+def get_vars(host):
+    """
+
+    """
+    base_dir, molecule_dir = base_directory()
+
+    pp.pprint(" => '{}' / '{}'".format(base_dir, molecule_dir))
+
+    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
+    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
+    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
 
 
 @pytest.mark.parametrize("dirs", [
@@ -25,7 +66,6 @@ def test_directories(host, dirs):
 @pytest.mark.parametrize("files", [
     "/etc/default/beanstalkd",
     "/usr/bin/beanstalkd",
-
 ])
 def test_files(host, files):
     f = host.file(files)
@@ -36,3 +76,23 @@ def test_files(host, files):
 def test_user(host):
     assert host.user("beanstalkd").exists
     assert host.group("beanstalkd").exists
+
+
+def test_service(host):
+    service = host.service("beanstalkd")
+
+    if(service.__class__.__name__ != 'SysvService'):
+        assert service.is_enabled is True
+
+    assert service.is_running is True
+
+
+@pytest.mark.parametrize("ports", [
+    '127.0.0.1:11300'
+])
+def test_open_port(host, ports):
+    for i in host.socket.get_listening_sockets():
+        print(i)
+
+    socket = host.socket("tcp://{}".format(ports))
+    assert socket.is_listening
